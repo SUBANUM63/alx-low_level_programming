b97fc96a108b08e80f71914ae3aa26ea01f2eebb
Zeinu Kedir <zeinuka641@gmail.com>
2023-09-13 13:05:44 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
+
 /**
- * main - Prints _putchar as a message.
- *
- * Return: Always 0 (Success)
+ * main - print _putchar, followed by new line
+ * Return: 0
  */
 
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	
-	
-	for (count = 0; count < sizeof(str); count++)
-	{
-		_putchar(str[count]);
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
