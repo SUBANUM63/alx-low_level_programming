4600fd390c0df60289c672d6511b235af0cf3e22
Zeinu Kedir <zeinuka641@gmail.com>
2023-09-20 09:31:42 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encode string into 1337 leet
+ * @s: string to manipulate
+ * Return: string
+ */
+
+char *leet(char *s)
+{
+
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	int i, j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		for (i = 0; a[i] != '\0'; i++)
+		{
+			if (s[j] == a[i])
+				s[j] = b[i];
+		}
+	}
+
+	return (s);
+}
