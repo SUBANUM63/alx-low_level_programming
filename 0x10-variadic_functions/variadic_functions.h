a75365710fdc7de39f6a7976f475d9b92b633e26
Zeinu Kedir <zeinuka641@gmail.com>
2023-10-12 14:30:04 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,13 @@
-File 0-sum_them_all.c is a C Program file containing function that returns the sum of all its parameters.
+#ifndef VAR_FUNC_H
+#define VAR_FUNC_H
 
-File 1-print_numbers.c is a C Program file containing functiona function that prints numbers, followed by a new line.
+#include <stdio.h>
+#include <stdarg.h>
 
-File 2-print_strings.c is a C Program file containing functiona function that prints strings, followed by a new line.
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
-File 3-print_all.c is a C Program file containing functiona function that prints anything.
-
-File variadic_functions.h is the header file that contains all the function prototypes.
+#endif
 
