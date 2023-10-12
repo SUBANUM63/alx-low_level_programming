3951f5152fd2268ad519ddd6397e5334a2051af5
Zeinu Kedir <zeinuka641@gmail.com>
2023-10-12 14:23:30 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+File 0-sum_them_all.c is a C Program file containing function that returns the sum of all its parameters.
+
+File 1-print_numbers.c is a C Program file containing functiona function that prints numbers, followed by a new line.
+
+File 2-print_strings.c is a C Program file containing functiona function that prints strings, followed by a new line.
+
+File 3-print_all.c is a C Program file containing functiona function that prints anything.
+
+File variadic_functions.h is the header file that contains all the function prototypes.
+
