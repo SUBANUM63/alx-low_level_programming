fbfe929bdb1f7a33b7e56d64832f0dff12390bcd
Zeinu Kedir <zeinuka641@gmail.com>
2023-10-20 12:04:04 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,10 @@
-section .text
-    global _start
+		global	main
+		extern	printf
+main:
+		mov	edi, format
+		xor	eax, eax
+		call	printf
+		mov	eax, 0
+		ret
+format: db 'Hello, Holberton\n',0
 
-_start:
-    mov rax, 0x2000004 ; write system call
-    mov rdi, 1 ; stdout file descriptor
-    mov rsi, msg ; address of string 
-    mov rdx, 14 ; number of bytes to write
-    syscall
-
-    mov rax, 60 ; exit system call
-    mov rdi, 0 ; exit code 0
-    syscall 
-
-section .data
-    msg: db "Hello, Holberton", 10 ; string to print plus newline
